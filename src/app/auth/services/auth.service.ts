e17a7699c4bf8b8dd5d3a04bc4cec65af0232d96
couettou <delignyb.pro@gmail.com>
2021-01-23 15:49:00 +0100

    feat(auth): create authService with login function and use environment

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { shareReplay } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  logUser(formUser: any) {
+    const user = formUser;
+
+    return this.http
+      .post<any>(environment.localUrl + 'auth/login', user)
+      .subscribe((token) => {
+        localStorage.setItem('jwt_token', token['access_token']);
+      });
+    // TODO find how to import .shareReplay();
+  }
+}
