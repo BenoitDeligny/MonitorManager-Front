f2fe93b0c8db9f516e0cb3c4da917ad91a8403d9
couettou <delignyb.pro@gmail.com>
2021-01-23 15:48:12 +0100

    feat(loginPage): fill the page and imports

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { AuthComponent } from './auth.component';
 import { LoginComponent } from './pages/login/login.component';
 
+import { FormsModule } from '@angular/forms';
+
+// * Imports de la bibliothèque PRIMENG
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
-  declarations: [AuthComponent, LoginComponent],
+  declarations: [LoginComponent, SignupComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    AuthRoutingModule,
+    FormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    RippleModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, NgForm } from '@angular/forms';
+import { PrimeNGConfig } from 'primeng/api';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private primengConfig: PrimeNGConfig,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
+    // * active l'effet blanc au clique du bouton
+    this.primengConfig.ripple = true;
   }
 
+  onSubmit(loginForm: NgForm) {
+    this.authService.logUser(loginForm.value);
+  }
 }
