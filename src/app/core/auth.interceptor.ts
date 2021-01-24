e5e7d6719fa42d67a331eeea805a0f68fa1cc550
couettou <delignyb.pro@gmail.com>
2021-01-24 15:54:21 +0100

    feat(http): create and fille the authInterceptor

---
+++ src/app/core/auth.interceptor.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    request = request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${localStorage.getItem('jwt_token')}`,
+      },
+    });
+
+    return next.handle(request).pipe(
+      tap(
+        (event: HttpEvent<any>) => {
+          if (event instanceof HttpResponse) {
+          }
+        },
+        (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status === 401) {
+              alert('Bad credentials');
+            }
+          }
+        }
+      )
+    );
+  }
+}
