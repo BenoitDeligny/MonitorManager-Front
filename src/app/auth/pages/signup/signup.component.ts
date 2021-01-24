d5af70c2ce4f30336cbeafb3afdc3091719c609c
couettou <delignyb.pro@gmail.com>
2021-01-24 17:22:16 +0100

    feat(signupPage): fille the page

--- src/app/auth/pages/signup/signup.component.ts
+++ src/app/auth/pages/signup/signup.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PrimeNGConfig } from 'primeng/api';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private primengConfig: PrimeNGConfig,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
+    // * active l'effet blanc au clique du bouton
+    this.primengConfig.ripple = true;
   }
 
+  createUser(signupForm: NgForm) {
+    if (signupForm.valid) {
+      this.authService.createUser(signupForm.value);
+    } else {
+      alert('It miss some informations ! (Or you are trying to cheat...)');
+    }
+  }
 }
