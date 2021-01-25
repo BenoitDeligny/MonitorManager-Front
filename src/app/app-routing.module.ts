2d0824148a98412bf91eb8ff0923dafcb4b54f6b
couettou <delignyb.pro@gmail.com>
2021-01-25 11:13:00 +0100

    chore(routing): add wild route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,10 +12,10 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
-  /* {
+  {
     path: '**',
-    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
-  }, */
+    component: HomeComponent,
+  },
 ];
 
 @NgModule({
