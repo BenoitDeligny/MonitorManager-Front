24a5df0a8645f71cf04e26db7ec2a578988955a6
couettou <delignyb.pro@gmail.com>
2021-01-26 01:18:46 +0100

    feat(customersModule): create and fill all in the customerModule

---
+++ src/app/customers/services/customers.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { Customer } from 'src/app/shared/models/customer';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomersService {
+  constructor(private http: HttpClient) {}
+
+  getAllCustomers(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(environment.localUrl + 'customers');
+  }
+
+  saveCustomer(user: Customer) {
+    this.http.post(environment.localUrl + 'customers', user).subscribe();
+  }
+
+  updateCustomer(user: Customer) {
+    this.http.put(environment.localUrl + 'customers', user).subscribe();
+  }
+
+  deleteCustomer(id: number): void {
+    this.http.delete(environment.localUrl + `customers/${id}`).subscribe();
+  }
+}
